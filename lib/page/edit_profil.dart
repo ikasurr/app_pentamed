3bd569b814dec6c523c710d8c6c25805119f9675
ika suryanti <ikasuryantii24@gmail.com>
2025-06-11 12:30:22 +0700

    menambah edit

---
+++ lib/page/edit_profil.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class EditProfil extends StatefulWidget {
+  const EditProfil({super.key});
+
+  @override
+  State<EditProfil> createState() => _EditProfilState();
+}
+
+class _EditProfilState extends State<EditProfil> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

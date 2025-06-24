1446e41ca93b94814c8d744dbe2428c1fb6935b1
rahmankur123 <rahmankur907@gmail.com>
2025-06-24 15:10:29 +0700

    halaman edit

--- lib/halaman_awal.dart
+++ lib/halaman_awal.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'screens/auth/login_screen.dart';
+import 'screens/obat/obat_screen.dart';
+import 'screens/profile/profile_screen.dart';
 
 class HalamanAwal extends StatefulWidget {
   const HalamanAwal({super.key});
@@ -11,20 +14,23 @@ class HalamanAwal extends StatefulWidget {
 class _HalamanAwalState extends State<HalamanAwal> {
   int _currentIndex = 1;
 
-  //final List<Widget> _pages = [Obat(), Dashboard(), Laporan(), Profile()];
+  final List<Widget> _pages = [
+    ObatListScreen(),
+    LoginScreen(),
+    ProfileScreen(),
+  ];
 
   final items = <Widget>[
     Icon(Icons.medical_information, size: 30),
     Icon(Icons.home, size: 30),
     Icon(Icons.article_outlined, size: 30),
-    Icon(Icons.person, size: 30),
   ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.amber,
       appBar: AppBar(title: Text("Hallo, Admin")),
-      //body: _pages[_currentIndex],
+      body: _pages[_currentIndex],
       bottomNavigationBar: CurvedNavigationBar(
         index: _currentIndex,
         items: items,
